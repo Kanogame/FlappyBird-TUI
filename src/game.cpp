e2493b3518579ae18f3d3f72b316e2ec7a91ad7a
kanogame <wasdqert1222@gmail.com>
2023-07-09 20:56:27 +0700

    test 2

--- src/game.cpp
+++ src/game.cpp
@@ -4,9 +4,15 @@
 
 namespace flappyBird {
     Game::Game(GameInfo gameInfo) {
-        for (int i = 0; i < gameInfo.HEIGHT; i++) {
-            for (int j = 0; j < gameInfo.WIDTH; j++) {
-                std::cout << "#";
+        for (int i = 0; i < gameInfo.WIDTH; i++) {
+            for (int j = 0; j < gameInfo.HEIGHT; j++) {
+                if (i % 6 == 0) {
+                    std::cout << "#";
+                } else if (j % 6 == 0) {
+                    std::cout << "#";
+                } else {
+                    std::cout << " ";
+                }
             }
         }
     }
