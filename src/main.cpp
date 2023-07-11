b7a513b46d75ae2a046944215f598367977f4e2a
kanogame <wasdqert1222@gmail.com>
2023-07-11 20:37:43 +0700

    fuck it, rewrite in ncurses

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
-#include "Game/game.hpp"
 
 #include <iostream> 
-#include <sys/ioctl.h>
-#include <unistd.h>
+#include <ncurses.h>
 
 int main() {
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    initscr();
+    refresh();
 
-    flappyBird::GameInfo gameinfo;
-    gameinfo.ClientWindow = flappyBird::Window{w.ws_col, w.ws_row};
-    gameinfo.BirdPosition = 10;
-
-    flappyBird::Game game{gameinfo};
-
-    int test;
-    std::cin >> test;
+    WINDOW *board = newwin(LINES - 1, COLS - 1, 0, 0);
+    mvwprintw(board, 2, 2, "%d:%d", LINES, COLS);
+    box(board, 0, 0);
+    wrefresh(board);
+    getch();
+    endwin();
     return 0;
 }
